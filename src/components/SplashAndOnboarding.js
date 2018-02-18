82482b0324ad868e05161279f0972629423ec8ea
Eleanor Weigert <eweigert@gmail.com>
2018-02-18 14:03:07 -0800

    spruce up onboarding layout

--- src/components/SplashAndOnboarding.js
+++ src/components/SplashAndOnboarding.js
@@ -4,19 +4,6 @@ import Slider from 'react-slick';
 import { browserHistory } from 'react-router';
 import { Button } from 'react-bootstrap';
 
-// SplashAndOnboarding page component
-
-function SampleNextArrow(props) {
-  const {className, onClick} = props
-  return (
-    <div
-      className={className}
-      style={{ display: 'block', background: 'rgba(0,0,0,0.5)'}}
-      onClick={onClick}
-    ></div>
-  );
-}
-
 export default class SplashAndOnboarding extends React.Component {
   componentDidMount() {
    browserHistory.replace('/');
@@ -30,22 +17,44 @@ export default class SplashAndOnboarding extends React.Component {
       slidesToShow: 1,
       slidesToScroll: 1,
       swipeToSlide: true,
-      arrows: true,
-      nextArrow: <SampleNextArrow />,
-      prevArrow: <SampleNextArrow />
+      centerMode: true,
+      arrows: false
     };
+
     return (
       <div className="page-splash">
           <Slider {...settings}>
-            <div><h3>1</h3></div>
-            <div><h3>2</h3></div>
-            <div><h3>3</h3></div>
-            <div><h3>4</h3></div>
-            <div><h3>5</h3></div>
-            <div>
+          <div className="slide-wrapper">
+            <div className="slide-container">
+              <h3>1</h3>
+            </div>
+          </div>
+          <div className="slide-wrapper">
+            <div className="slide-container">
+              <h3>2</h3>
+            </div>
+          </div>
+          <div className="slide-wrapper">
+            <div className="slide-container">
+              <h3>3</h3>
+            </div>
+          </div>
+          <div className="slide-wrapper">
+            <div className="slide-container">
+              <h3>4</h3>
+            </div>
+          </div>
+          <div className="slide-wrapper">
+            <div className="slide-container">
+              <h3>5</h3>
+            </div>
+          </div>
+          <div className="slide-wrapper">
+            <div className="slide-container">
               <h3> last</h3>
               Hi I'm a splash page.
               <Button onClick={this.props.hide}>Click me to dismiss</Button>
+              </div>
             </div>
           </Slider>
         </div>
