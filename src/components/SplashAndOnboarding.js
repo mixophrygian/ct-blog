741d2d820d943406e2312a32289f3fd166c2d886
Eleanor Weigert <eweigert@gmail.com>
2018-02-21 21:39:00 -0800

    add working text and cute svgs to slides

--- src/components/SplashAndOnboarding.js
+++ src/components/SplashAndOnboarding.js
@@ -3,6 +3,10 @@ import React, { PropTypes } from 'react';
 import Slider from 'react-slick';
 import { browserHistory } from 'react-router';
 import { Button } from 'react-bootstrap';
+import Flowers from './art/Flowers.svg';
+import AirBalloon from './art/AirBalloon.svg';
+import Monkeys from './art/Monkeys.svg';
+import FingerPuppets from './art/FingerPuppets.svg';
 
 export default class SplashAndOnboarding extends React.Component {
   componentDidMount() {
@@ -26,34 +30,47 @@ export default class SplashAndOnboarding extends React.Component {
           <Slider {...settings}>
           <div className="slide-wrapper">
             <div className="slide-container">
-              <h3>1</h3>
+              <h3>Automatic</h3>
+              <h3>Thought</h3>
+              <h3>Journal</h3>
+              <br/>
+              <p><i>"Your thoughts create your emotions. Therefore, your emotions cannot prove that your thoughts are accurate. - David Burns.</i></p>
+              <br/>
+              <p>Welcome.  The Automatic Thought Journal is a private micro blog for deconstructing cognitive distortions.  It's free, has no ads, no analytics, and your data is only saved locally to this browser.</p>
+              <img src={Flowers} />
             </div>
           </div>
           <div className="slide-wrapper">
             <div className="slide-container">
-              <h3>2</h3>
+              <h3>Cognitive Distortions?</h3>
+              <br/>
+              <p>This micro blog was created to be used as a companion tool for the book <b>Feeling Good</b> by David Burns.</p>
+              <br/>
+              <p>In this book he outlines an exercize in which someone who has experienced an automatic thought can methodically break it down into manageable pieces.</p>
+              <img src={AirBalloon} />
             </div>
           </div>
           <div className="slide-wrapper">
             <div className="slide-container">
-              <h3>3</h3>
+              <h3>How do I use this?</h3>
+              <br/>
+              <p></p>
+              <br/>
+              <p>In this book he outlines an exercize in which someone who has experienced an automatic thought can methodically break it down into manageable pieces.</p>
+              <br/>
+              <p>By noticing the pattern that the thoughts follow, you start to see the distortions for what they are - distortions.</p>
+              <img src={Monkeys} />
             </div>
           </div>
           <div className="slide-wrapper">
             <div className="slide-container">
-              <h3>4</h3>
-            </div>
-          </div>
-          <div className="slide-wrapper">
-            <div className="slide-container">
-              <h3>5</h3>
-            </div>
-          </div>
-          <div className="slide-wrapper">
-            <div className="slide-container">
-              <h3> last</h3>
-              Hi I'm a splash page.
-              <Button onClick={this.props.hide}>Click me to dismiss</Button>
+            <h3>Try it out!</h3>
+            <br/>
+            <p>We don't save your data, in fact it never leaves this phone.</p>
+            <br/>
+            <p>Read more about the cognitive distortions from the menu and in David Burns' <b>Feeling Good</b>.</p>
+            <img src={FingerPuppets} />
+              <Button onClick={this.props.hide}>Got it!</Button>
               </div>
             </div>
           </Slider>
