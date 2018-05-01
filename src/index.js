dcec7b520b1c7064b45c15eb0b431b0aa2910ede
Scott Thorpe <sthorpe@gmail.com>
2018-05-01 13:11:39 -0700

    Updated linter to work with yarn lint or npm lint

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import "babel-polyfill";
 import React from "react";
 import ReactDOM from "react-dom";
+alksdjf;
 import { Provider } from "react-redux";
 import { store } from "./store.js";
 import { router } from "./router.js";
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -29,3 +29,5 @@ module.exports.plugins.push(
   }),
   new UglifyWebpackPlugin()
 );
+
+module.export.mode = 'development';
