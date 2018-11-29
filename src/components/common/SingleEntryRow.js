94f7fa8af8e9eb5d78fdda720e56131ca087462d
Eleanor Weigert <eweigert@gmail.com>
2018-11-28 19:36:46 -0800

    complete home page restyling, with more link

--- src/components/common/SingleEntryRow.js
+++ src/components/common/SingleEntryRow.js
@@ -8,6 +8,7 @@ export default class SingleEntryRow extends React.Component {
     super(props);
     this.viewEntry = this.viewEntry.bind(this);
     this.renderDistortionLabels = this.renderDistortionLabels.bind(this);
+    this.truncateAndAddMoreLink = this.truncateAndAddMoreLink.bind(this);
   }
 
   viewEntry(e) {
@@ -22,19 +23,36 @@ export default class SingleEntryRow extends React.Component {
     if (!list) return;
     return (
       <div className="distortionLabelContainer">
-        {list.map(label => <div className={`${label} distortionLabel`}>{displayLabel(label)}</div>)}
+        {list.map(label => (
+          <div key={label} className={`${label} distortionLabel`}>
+            {displayLabel(label)}
+          </div>
+        ))}
       </div>
     );
+  }
+
+  truncateAndAddMoreLink(text) {
+    if (text.length > 210) {
+      const truncatedText = text.substring(0, 210) + "...";
+      return (
+        <span>
+          {truncatedText}
+          <button className="moreLink" onClick={this.viewEntry}>
+            more
+          </button>
+        </span>
+      );
+    }
   }
 
   render() {
     const { entry } = this.props;
     const date = formatDate(entry.date);
-
     return (
       <div className="tableRow " data-id={entry.id} onClick={this.viewEntry}>
         <div className="entryDate">{date}</div>
-        <div className="entryTitle">{entry.situation}</div>
+        <div className="entryTitle">{this.truncateAndAddMoreLink(entry.situation)}</div>
         {this.renderDistortionLabels(entry.cognitiveDistortions)}
       </div>
     );
