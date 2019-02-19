c9f165055f458adb812fc6610035281c87585040
Eleanor Weigert <eweigert@gmail.com>
2019-02-18 20:35:07 -0800

    minor tweaks to No Entries state

--- src/components/common/NoEntries.js
+++ src/components/common/NoEntries.js
@@ -12,16 +12,18 @@ export default class NoEntries extends React.Component {
       <div className="empty-container">
         <h4>Welcome</h4>
         <p className="quote">
-          Ideally this section will contain some kind of introduction or high level summary of this
-          if you didn't fully understand the onboarding flow. Ultimaltely it should funnel you into
-          either an explanation or a new entry. Last updated 2/4 8:10pm PST
+          This micro blog was created to be used as a companion tool for the book{" "}
+          <b>Feeling Good</b>
+          {} by David Burns. In this book he outlines an exercize in which someone who has
+          experienced an automatic thought can methodically break it down into manageable pieces.{" "}
         </p>
         <br />
         <Link className="whatIsThis" to={"/about"}>
-          What is this?
+          How do I use this?
         </Link>
         <br />
         <br />
+        <p> Last updated 2/18/19 8:10pm PST</p>
       </div>
     );
   }
